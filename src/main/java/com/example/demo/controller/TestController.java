5ea09c93766a2eb0c4dd07fcb18e32ab2ffa7ec5
noah <dbwlsrla002@naver.com>
2024-05-20 10:36:29 +0900

    back 13 완료

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -1,9 +1,16 @@
 package com.example.demo.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+
+import com.example.demo.dto.ResponseDTO;
 import com.example.demo.dto.TestRequestBodyDTO;
+
 
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -13,12 +20,20 @@ import org.springframework.web.bind.annotation.RequestBody;
 public class TestController {
 	
 	@GetMapping
-	public String testController() {
-		return "Hello World!(안녕하세요!)";
+	public String TestController() {
+		return "Hello World!";
 	}
 	
 	@GetMapping("/testRequestBody")
 	public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {//여기 코드 500 internal error 나서 이걸로 변경함P.82
 		return "Hello World! ID "+ testRequestBodyDTO.getId()+" Message : "+testRequestBodyDTO.getMessage();
+	}
+	@GetMapping("/testResponseBody")
+	public ResponseDTO<String>testControllerResponseBody(){
+		List<String> list = new ArrayList<String>();
+		list.add("Hello World! I'm Responsive DTO");
+		list.add("See you!");
+		ResponseDTO<String> response = ResponseDTO.<String>builder().data(list).build();
+		return response;
 	}
 }
