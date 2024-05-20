8b21284bb6c79da37abc88aee270ede29075ce2b
noah <dbwlsrla002@naver.com>
2024-05-20 10:10:18 +0900

    back 12 완료

--- src/main/java/com/example/demo/controller/TestController.java
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -3,9 +3,9 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import com.example.demo.dto.TestRequestBodyDTO;
 
+import org.springframework.web.bind.annotation.RequestBody;
 
 @RestController
 @RequestMapping("test")
@@ -16,13 +16,9 @@ public class TestController {
 	public String testController() {
 		return "Hello World!(안녕하세요!)";
 	}
-	@GetMapping("/{id}")
-	public String testControllerWithPathVariables(@PathVariable(value="id")int id) {//여기 코드 500 internal error 나서 이걸로 변경함P.82
-		return "Hello World! ID "+ id;
-	}
 	
-	@GetMapping("/Param")
-	public String testControllerRequestParam(@RequestParam(value="id")int id) {
-		return "Hello World! ID param "+id;
+	@GetMapping("/testRequestBody")
+	public String testControllerRequestBody(@RequestBody TestRequestBodyDTO testRequestBodyDTO) {//여기 코드 500 internal error 나서 이걸로 변경함P.82
+		return "Hello World! ID "+ testRequestBodyDTO.getId()+" Message : "+testRequestBodyDTO.getMessage();
 	}
 }
